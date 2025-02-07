39c17990131e7c65d3f741c02a1b61b79287cd13
maat0001kea <maat0001@stud.kea.dk>
2025-02-07 15:06:52 +0100

    nyt

---
+++ Name-parts/script.js
@@ -0,0 +1,15 @@
+const str = "Jack Erick Mansion.";
+
+const firstName = str.substring(str.indexOf(""), str.indexOf(" "));
+console.log(firstName);
+// Expected output: "fox"
+
+const middleName = str.substring(str.indexOf(" "), str.lastIndexOf(" "));
+console.log(middleName);
+// Expected output: "fox"
+
+const lastName = str.substring(str.lastIndexOf(" "));
+console.log(lastName);
+// Expected output: "fox"
+
+console.log(firstName, middleName, lastName);
