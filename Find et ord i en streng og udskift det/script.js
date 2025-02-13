008c0ce83403733975ca03972a9625ed409fb0e7
maat0001kea <maat0001@stud.kea.dk>
2025-02-13 11:08:46 +0100

    nyt

--- Find et ord i en streng og udskift det/script.js
+++ Find et ord i en streng og udskift det/script.js
@@ -3,8 +3,11 @@ let theText =
 
 // Løsning med split / join
 let newText1 = theText.split("ord").join("ting");
-console.log("Med split/join:", newText1);
 
 // Løsning med replaceAll()
 let newText2 = theText.replaceAll("ord", "ting");
-console.log("Med replaceAll():", newText2);
+
+// Indsætter resultaterne i HTML
+document.getElementById("original-text").innerText = theText;
+document.getElementById("split-join-result").innerText = newText1;
+document.getElementById("replaceAll-result").innerText = newText2;
